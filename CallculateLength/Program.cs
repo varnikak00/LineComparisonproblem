109d0b54db383d094c00bf1325ca42817e868002
varnikak00 <varnikak7070@gmail.com>
2021-09-27 21:27:32 +0530

    program of callculatelength

---
+++ CallculateLength/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CallculateLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line comparison computation");
+
+            double line1, line2;
+            Program p = new Program();
+            p.takeCartesiaPoint();
+            line1 = p.computeDistance();
+            Console.WriteLine("Line distance is " + line1);
+
+            p.takeCartesiaPoint();
+            line2 = p.computeDistance();
+            Console.WriteLine("Line distance is " + line2);
+
+            Console.WriteLine(p.DisplayLinesRelation(line1, line2));
+        }
+
+        private double computeDistance()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void takeCartesiaPoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool DisplayLinesRelation(double line1, double line2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
